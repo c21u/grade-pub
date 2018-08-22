2c793a9f0997358fdf0bc106319a3a94fb62d445
Emily Reese <ereese3@mail.gatech.edu>
2018-08-22 15:26:23 -0400

    Improve tests a little

--- tests.js
+++ tests.js
@@ -3,7 +3,20 @@ const test = require('tape');
 
 const app = require('./app');
 
-test('First test', (assert) => {
+test('Can POST to get authenticated', (assert) => {
+  request(app)
+  .post('/')
+  .send('username=admin')
+  .send('password=1234')
+  .expect(302)
+  .expect('Location', '/secret')
+  .end((err, res) => {
+    assert.error(err, 'No error');
+    assert.end();
+  });
+});
+
+test('GET / should be protected', (assert) => {
   request(app)
   .get('/')
   .expect(401)
