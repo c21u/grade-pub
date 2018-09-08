1ef2e18d56d4c6fdfe54fc0ef3bd3cf82773a4e7
Emily Reese <ereese3@mail.gatech.edu>
2018-09-07 20:07:24 -0400

    Split the webpack config
    Create separate configs for dev and prod.

---
+++ webpack.common.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './client/src/index.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+            ],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HTMLWebpackPlugin({
+      title: 'Production --',
+    }),
+  ],
+};
