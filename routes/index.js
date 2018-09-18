90624b452bdc06583640f16512e878e7a25efe5f
Emily Reese <ereese3@mail.gatech.edu>
2018-09-18 10:02:04 -0400

    Fix and lint index routes

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,18 @@
-let express = require('express');
+let express = require("express");
 // eslint-disable-next-line new-cap
 let router = express.Router();
-let jwt = require('jsonwebtoken');
+let jwt = require("jsonwebtoken");
 let jwtMiddleware = require("../lib/jwt");
-const passport = require('../lib/passport');
+const passport = require("../lib/passport");
 
 // Passport initialized here but actually used as passport.authenticate()
 router.use(passport.initialize());
 let passportStrategy = require("../config")["passportStrategy"];
+
+// health check endpoint
+router.get("/z", (req, res, next) => {
+  res.sendStatus(200);
+});
 
 router.post(
   "/",
@@ -17,25 +22,18 @@ router.post(
       const jwtSecret = require("../config")["jwtSecret"];
       const expiresIn = 60 * 60 * 24 * 180; // 180 days
       const token = jwt.sign(req.user, jwtSecret, { expiresIn });
-      res.redirect(`/?token=${token}`);
+      let clientURL = require("../config")["clientURL"];
+      res.redirect(`${clientURL}?token=${token}`);
     } else {
       res.sendStatus(401);
     }
   }
 );
 
-// health check endpoint
-router.get('/z', (req, res, next) => {
-  res.sendStatus(200);
-});
-
 router.use(jwtMiddleware);
 
-router.get(
-  '/',
-  (req, res, next) => {
-    res.render('index', {title: 'Express'});
-  }
-);
+router.get("/", (req, res, next) => {
+  res.sendFile("index.html", { root: "dist" });
+});
 
 module.exports = router;
