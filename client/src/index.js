699d8ecb75d65831a8e348fb34834a4da638c43e
Emily Reese <ereese3@mail.gatech.edu>
2018-09-25 15:09:48 -0400

    Use React router.
    Include react-router to pick which components to display, depending on
    whether or not a valid JWT was found.

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,57 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import theme from "@instructure/ui-themes/lib/canvas";
-import Heading from "@instructure/ui-elements/lib/components/Heading";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import qs from "qs";
+import jwtDecode from "jwt-decode";
 
 theme.use();
 
-let Greeting = () => {
+let context = {};
+
+/**
+ * Get the JWT from the `token` query parameter. The jwtDecode() will throw
+ * InvalidTokenError if it cannot decode the JWT, otherwise we copy the token
+ * and decoded values into the context object.
+ */
+let grabJWT = () => {
+  try {
+    let jwt = qs.parse(window.location.search, {
+      ignoreQueryPrefix: true
+    }).token;
+    context.lti = jwtDecode(jwt);
+    context.jwt = jwt;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+let ProtectedRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      !!context.jwt ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to={{ pathname: "/default" }} />
+      )
+    }
+  />
+);
+
+let App = () => {
+  grabJWT();
   return (
-    <div>
-      <Heading>Hello world3</Heading>
-    </div>
+    <Router>
+      <div>
+        <Route path="/default" component={defaultRoute} />
+        <ProtectedRoute exact path="/" component={appRoute} />
+      </div>
+    </Router>
   );
 };
 
-ReactDOM.render(<Greeting />, document.getElementById("lti_root"));
+let defaultRoute = () => <h1>Default unprotected route</h1>;
+let appRoute = () => <h1>Protected app route</h1>;
+
+ReactDOM.render(<App />, document.getElementById("lti_root"));
