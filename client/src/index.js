b96321cf5c32dacdf5c1b0d0ab8d9a719fb46475
Emily Reese <ereese3@mail.gatech.edu>
2018-09-27 11:11:02 -0400

    Create Instructions component.
    Use Instructure-UI components to build the instruction text.

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import theme from "@instructure/ui-themes/lib/canvas";
+import Heading from "@instructure/ui-elements/lib/components/Heading";
+import Link from "@instructure/ui-elements/lib/components/Link";
+import List, { ListItem } from "@instructure/ui-elements/lib/components/List";
+import Text from "@instructure/ui-elements/lib/components/Text";
+import View from "@instructure/ui-layout/lib/components/View";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import qs from "qs";
 import jwtDecode from "jwt-decode";
@@ -40,6 +45,89 @@ let ProtectedRoute = ({ component: Component, ...rest }) => (
   />
 );
 
+let Instructions = () => {
+  return (
+    <View as="div" padding="large">
+      <Heading>Instructions</Heading>
+      <View as="div" maxWidth="80%" margin="medium">
+        <Text>
+          <strong>Step 1:</strong> Export your grades from Canvas. If you
+          encounter issues with this step, contact the Digital Learning Team at{" "}
+          <Link href="mailto:canvas@gatech.edu">canvas@gatech.edu</Link>.
+        </Text>
+        <List itemSpacing="small" margin="small">
+          <ListItem>
+            <Text>
+              In order to use this tool, you will first need to enable and set a
+              grading scheme for the final course grade in Canvas.{" "}
+              <Link
+                href="http://canvas.gatech.edu/set-canvas-final-grade-reporting"
+                target="_blank"
+              >
+                Click here to find the grading scheme instructions.
+              </Link>
+            </Text>
+          </ListItem>
+          <ListItem>
+            <Text>
+              Click the button below to export a Banner-ready spreadsheet of
+              grades and save the file to your computer. Please note: You can
+              reuse the same spreadsheet file to upload grades for multiple
+              sections in Step 2.
+            </Text>
+          </ListItem>
+        </List>
+      </View>
+      <View as="div" maxWidth="80%" margin="medium">
+        <Text>
+          <strong>Step 2:</strong> Upload your grades to Banner. If you
+          encounter issues with this step, contact the {"Registrar's"} Office.
+          (see{" "}
+          <Link
+            href="https://registrar.gatech.edu/faculty-and-staff/grading-and-grade-entry"
+            target="_blank"
+          >
+            Grading and Grade Entry Information
+          </Link>
+          )
+        </Text>
+        <List itemSpacing="small" margin="small">
+          <ListItem>
+            <Text>
+              Go to{" "}
+              <Link href="https://buzzport.gatech.edu/" target="_blank">
+                BuzzPort
+              </Link>{" "}
+              and login using your GT Account.
+            </Text>
+          </ListItem>
+          <ListItem>
+            <Text>
+              Click {"Registration - OSCAR"} in the{" "}
+              {"Registration and Student Services"} section.
+            </Text>
+          </ListItem>
+          <ListItem>
+            <Text>Click {"Faculty Services."}</Text>
+          </ListItem>
+          <ListItem>
+            <Text>Select the new Faculty Grade Entry option.</Text>
+          </ListItem>
+          <ListItem>
+            <Text>
+              Upload the grades spreadsheet (see Step 1) to Banner. If your
+              spreadsheet contains multiple sections, you will need to upload it
+              one time for each section. During the upload process, there will
+              be errors about any records in the grades spreadsheet which do not
+              match the current section. This is expected behavior.
+            </Text>
+          </ListItem>
+        </List>
+      </View>
+    </View>
+  );
+};
+
 let App = () => {
   grabJWT();
   if (!!context.jwt) {
@@ -60,13 +148,12 @@ let App = () => {
     <Router>
       <div>
         <Route path="/default" component={defaultRoute} />
-        <ProtectedRoute exact path="/" component={appRoute} />
+        <ProtectedRoute exact path="/" component={Instructions} />
       </div>
     </Router>
   );
 };
 
 let defaultRoute = () => <h1>Default unprotected route</h1>;
-let appRoute = () => <h1>Protected app route</h1>;
 
 ReactDOM.render(<App />, document.getElementById("lti_root"));
