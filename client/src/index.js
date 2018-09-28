054cc1f0aa9441c9251348051d34d05d44253d02
Emily Reese <ereese3@mail.gatech.edu>
2018-09-28 09:34:14 -0400

    Update the App component.
    Change it to be a class component.
    Add docstrings as dictated by linter.
    Make changes to the way fetch response is handled to get the data.
    Implement response status check, since fetch promises resolve happily
    even when the response has 4xx or 5xx error codes.

--- client/src/index.js
+++ client/src/index.js
@@ -40,33 +40,62 @@ let ProtectedRoute = ({ component: Component, ...rest }) => (
   />
 );
 
-let App = () => {
-  grabJWT();
-  if (!!context.jwt) {
-    fetch("/api/demo", {
-      headers: {
-        Authorization: `Bearer ${context.jwt}`
-      }
-    })
-      .then(response => {
-        console.log(response);
-      })
-      .catch(err => {
-        console.error(err);
-      });
+/** App component */
+class App extends React.Component {
+  /**
+   * App component constructor.
+   * @param {Object} props
+   */
+  constructor(props) {
+    super(props);
+
+    grabJWT();
+    if (!!context.jwt) {
+      window
+        .fetch("/api/demo", {
+          headers: {
+            Authorization: `Bearer ${context.jwt}`
+          }
+        })
+        .then(checkResponseStatus)
+        .then(response => response.json())
+        .then(json => console.log(json))
+        .catch(err => console.error(`fetch failed: ${err}`));
+    }
   }
 
-  return (
-    <Router>
-      <div>
-        <Route path="/default" component={defaultRoute} />
-        <ProtectedRoute exact path="/" component={appRoute} />
-      </div>
-    </Router>
-  );
+  /**
+   * Render App component with react-router
+   * @return {Object}
+   */
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route path="/default" component={defaultRoute} />
+          <ProtectedRoute exact path="/" component={appRoute} />
+        </div>
+      </Router>
+    );
+  }
 };
 
 let defaultRoute = () => <h1>Default unprotected route</h1>;
 let appRoute = () => <h1>Protected app route</h1>;
+
+/**
+ * Check for 200 OK status from response
+ * @param {Object} response
+ * @return {(Object|error)}
+ */
+let checkResponseStatus = response => {
+  if (response.status === 200) {
+    return response;
+  } else {
+    let err = new Error(response.statusText);
+    err.response = response;
+    throw err;
+  }
+};
 
 ReactDOM.render(<App />, document.getElementById("lti_root"));
