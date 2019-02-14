947b6ae2b2da0b79e6f7e589df45904c8a8f8691
D. Stuart Freeman <stuart.freeman@et.gatech.edu>
2019-02-14 16:38:22 -0500

    update spreadsheet instructions

--- client/src/spreadsheetInstructions.js
+++ client/src/spreadsheetInstructions.js
@@ -1,36 +1,47 @@
 // Instructions for the spreadsheet
 module.exports = [
-  ["Banner Grade Publishing"],
+  ["Grade Submission"],
   [],
   ["Instructions"],
   [],
   [],
-  ["Step 1: Export your grades from Canvas."],
+  ["Step 1: Export your grades from Canvas"],
   [],
   [
-    "",
-    "* In order to use this tool, you will first need to enable and set a grading scheme for the final course grade in Canvas. You can find those instructions at http://canvas.gatech.edu/grade-submission"
-  ],
-  [],
-  [
-    "",
-    "* Click the button below to export a Banner-ready spreadsheet of grades and save the file to your computer. Please note: You can reuse the same spreadsheet file to upload grades for multiple sections in Step 2."
-  ],
-  [],
-  [
-    "If you encounter issues with this step, contact the Digital Learning Team at at canvas@gatech.edu"
+    "In order to use the GT Grade Submission tool, you will first need to enable and set a grading scheme in Canvas:"
   ],
   [],
   [],
-  ["Step 2: Upload your grades to Banner."],
-  [],
+  ["  1 .Open your Course in Canvas"],
+  ['  2. Select "Settings" from the course navigation menu on the left.'],
   [
-    "",
-    "* Go to the Banner Faculty Grade Entry (FGE) module at https://fge.sis.gatech.edu/StudentFacultyGradeEntry"
+    '  3. Under the Course Details tab, select the check box next to "Enable Course Grading Scheme"'
+  ],
+  ['  4. Beneath the check box, click "View Grading Scheme"'],
+  [
+    '  5. In the View/Edit Grading Scheme window, click "Select Another Scheme"'
+  ],
+  ['  6. Select either "Midterm Grade" or "Final Grade"'],
+  ['  7. Click "Use This Grading Standard"'],
+  ["  8. Click Done"],
+  [
+    'Once you have completed these steps, select the "Banner Grade Publishing" button in the left-hand menu to export a Banner-ready spreadsheet of grades. You can use this same spreadsheet file to upload grades for multiple sections, using the directions in Step 2 below.'
   ],
   [],
   [
-    "If you encounter issues with this step, contact the Registrar's Office at comments@registrar.gatech.edu",
-    "For additional information from the Registrar, see Grading and Grade Entry Information at https://registrar.gatech.edu/faculty-and-staff/grading-and-grade-entry"
+    "If you encounter any issues with Step 1 above, please contact the Digital Learning Team at canvas@gatech.edu."
+  ],
+  [],
+  ["Step 2: Upload your grades to Banner"],
+  [],
+  [],
+  [
+    "  1. Go to the Banner Faculty Grade Entry (FGE) module at https://fge.sis.gatech.edu/StudentFacultyGradeEntry"
+  ],
+  [
+    "  2. Upload the spreadsheet you just exported from Canvas (see Step 1 above). If your spreadsheet contains multiple sections, you will need to upload the file once for each section. During the upload process, you will see error messages about any records in the grades spreadsheet which do not match the current section. This is expected behavior. More information on this step is available at https://registrar.gatech.edu/info/importing-grades"
+  ],
+  [
+    "If you encounter any issues with Step 2 above, please contact the Registrar's Office at comments@registrar.gatech.edu. For additional information from the Registrar, see https://registrar.gatech.edu/faculty-and-staff/grading-and-grade-entry."
   ]
 ];
