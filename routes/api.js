1040719478b9e16a852af95a2d334e71d68a79d2
D. Stuart Freeman <stuart.freeman@c21u.gatech.edu>
2023-05-08 10:37:05 -0400

    use async/await to remove chained promises and improve error logs

--- routes/api.js
+++ routes/api.js
@@ -11,6 +11,7 @@ const buzzapi = new BuzzApi({
   apiUser: buzzappid,
   apiPassword: buzzapipassword
 });
+const logger = require("../lib/logger");
 
 router.use(jwtMiddleware);
 
@@ -35,164 +36,163 @@ const chunk = function(arr, size) {
   }
   return chunks;
 };
-const getGrademodes = function(students) {
+
+const getGrademodes = async students => {
   const keyBy = (arr, element) =>
     arr.reduce((acc, curr) => ({ ...acc, [curr[element]]: curr }), {});
+
   const studentsById = keyBy(
-    students.map(student => ({
+    students.map(studen => ({
       ...student.user,
       section_id: student.sis_section_id
     })),
     "sis_user_id"
   );
   sisIdsChunks = chunk(
-    students.map(student => `(gtgtid=${student.user.sis_user_id})`),
+    students.map(studen => `(gtgtid=${student.user.sis_user_id})`),
     20
   );
-  return Promise.all(
-    sisIdsChunks.map(sisIds =>
-      buzzapi.post("central.iam.gted.people", "search", {
-        filter: `(|${sisIds.join("")})`,
-        requested_attributes: ["gtCourseInfoDetails1", "gtgtid"]
-      })
-    )
-  )
-    .then(responses => responses.reduce((acc, cur) => acc.concat(cur)))
-    .then(responses => {
-      return responses.map(response => {
-        const courseInfoDetails = response.gtCourseInfoDetails1;
-        let gradeMode;
-        if (courseInfoDetails) {
-          for (let i = 0; i < courseInfoDetails.length; i++) {
-            const courseInfoDetailsArray = courseInfoDetails[i].split("|");
-            const sectionId = `${courseInfoDetailsArray[1]}/${
-              courseInfoDetailsArray[2]
-            }`;
-            if (
-              studentsById[response.gtgtid].section_id &&
-              studentsById[response.gtgtid].section_id === sectionId
-            ) {
-              const modeCode = courseInfoDetailsArray.slice(-1).pop();
-              const modeMap = {
-                l: "Letter Grade",
-                p: "Pass / Fail",
-                a: "Audit"
-              };
-              gradeMode =
-                courseInfoDetailsArray[3] == "instructor"
-                  ? "Not available"
-                  : modeMap[modeCode] || "Not available";
-              break;
-            }
+  try {
+    const responses = await Promise.all(
+      sisIdsChunks.map(sisId =>
+        buzzapi.post("central.iam.gted.people", "search", {
+          filter: `(|${sisIds.join("")})`,
+          requested_attributes: ["gtCourseInfoDetails1", "gtgtid"]
+        })
+      )
+    );
+    const flatResponses = responses.reduce((acc, cur) => acc.concat(cur));
+    const results = flatResponses.map(respons => {
+      const courseInfoDetails = response.gtCourseInfoDetails1;
+      let gradeMode;
+      if (courseInfoDetails) {
+        for (let i = 0; i < courseInfoDetails.length; i++) {
+          courseInfoDetailsArray = courseInfoDetails[i].split("|");
+          const sectionId = `${courseInfoDetailsArray[1]}/${
+            courseInfoDetailsArray[2]
+          }`;
+          if (
+            studentsById[response.gtgtid].section_id &&
+            studentsById[response.gtgtid].section_id === sectionId
+          ) {
+            const modeCode = courseInfoDetailsArray.slice(-1).pop();
+            const modeMap = {
+              l: "Letter Grade",
+              p: "Pass / Fail",
+              a: "Audit"
+            };
+            gradeMode =
+              courseInfoDetailsArray[3] == "instructor"
+                ? "Not available"
+                : modeMap[modeCode] || "Not available";
+            break;
           }
-          return {
-            gtgtid: response.gtgtid,
-            gradeMode: gradeMode ? gradeMode : "Not available"
-          };
-        } else {
-          return {
-            gtgtid: response.gtgtid,
-            gradeMode: "Not available"
-          };
         }
-      });
-    })
-    .then(results => keyBy(results, "gtgtid"))
-    .catch(err => {
-      console.log(err);
+        return {
+          gtgtid: response.gtgtid,
+          gradeMode: gradeMode ? gradeMode : "Not available"
+        };
+      } else {
+        return {
+          gtgtid: response.gtgtid,
+          gradeMode: "Not available"
+        };
+      }
     });
+    return keyBy(results, "gtgtid");
+  } catch (err) {
+    logger.error(err);
+  }
 };
 
-router.get("/grades", (req, res, next) => {
+router.get("/grades", async (req, res, next) => {
   const canvas = canvasAPI.getCanvasContext(req);
   // getCanvasContext is imported from lib/canvas
 
-  canvas.api
-    .get(`courses/${canvas.courseID}/enrollments`, {
-      role: ["StudentEnrollment"]
-    }) // api call to canvas api, using canvas api documentation under enrollments
-    .then(students => {
-      return students.filter(s => s.sis_user_id);
-    }) // promise statement returns students
-    .then(async realStudents => {
-      // ** override feature ** - checks if override_grade exists, and if so, sets final_grade and current_grade  equal to override_grade
-      // if there is override grade, override value is equal to "Y" and if not, null
-      const gradeModes = await getGrademodes(realStudents);
-      return Promise.all(
-        realStudents.map(async s => ({
-          name: s.user.sortable_name,
-          currentGrade: s.grades.override_grade
-            ? s.grades.override_grade
-            : s.grades.current_grade,
-          finalGrade: s.grades.override_grade
-            ? s.grades.override_grade
-            : s.grades.final_grade,
-          unpostedFinalGrade: s.grades.unposted_final_grade,
-          unpostedCurrentGrade: s.grades.unposted_current_grade,
-          sisSectionID: s.sis_section_id,
-          gtID: s.user.sis_user_id,
-          course: req.user.custom_canvas_course_name,
-          override: s.grades.override_grade ? "Y" : null,
-          gradeMode: gradeModes[s.user.sis_user_id]
-        }))
+  try {
+    const students = await canvas.api.get(
+      `courses/${canvas.courseID}/enrollments`,
+      {
+        role: ["StudentEnrollment"]
+      }
+    ); // api call to canvas api, using canvas api documentation under enrollments
+    const realStudents = await students.filter(s.sis_user_id);
+    // ** override feature ** - checks if override_grade exists, and if so, sets final_grade and current_grade  equal to override_grade
+    // if there is override grade, override value is equal to "Y" and if not, null
+    const gradeModes = await getGrademodes(realStudents);
+    data = await Promise.all(
+      realStudents.map(async => ({
+        name: s.user.sortable_name,
+        currentGrade: s.grades.override_grade
+          ? s.grades.override_grade
+          : s.grades.current_grade,
+        finalGrade: s.grades.override_grade
+          ? s.grades.override_grade
+          : s.grades.final_grade,
+        unpostedFinalGrade: s.grades.unposted_final_grade,
+        unpostedCurrentGrade: s.grades.unposted_current_grade,
+        sisSectionID: s.sis_section_id,
+        gtID: s.user.sis_user_id,
+        course: req.user.custom_canvas_course_name,
+        override: s.grades.override_grade ? "Y" : null,
+        gradeMode: gradeModes[s.user.sis_user_id]
+      }))
+    );
+    res.send({ data });
+  } catch (err) {
+    res.status(500).send(err);
+    logger.error(err);
+  }
+});
+
+router.get("/gradeScheme", async (req, res) => {
+  const canvas = canvasAPI.getCanvasContext(req);
+
+  try {
+    const course = await canvas.api.get(`courses/${canvas.courseID}`);
+    let gs = {};
+
+    try {
+      gs = await canvas.api.get(
+        `accounts/self/grading_standards/${course.grading_standard_id}`
       );
-    })
-    .then(data => {
-      res.send({ data });
-    })
-    .catch(err => {
-      res.status(500).send(err);
-      console.log(JSON.stringify(err));
-    });
+    } catch (err) {
+      gs = await canvas.api.get(
+        `courses/${canvas.courseID}/grading_standards/${
+          course.grading_standard_id
+        }`
+      );
+    }
+    // Override titles for historic grade modes
+    if (gs.id === 40) {
+      gs.title = "Final Grade";
+    }
+    if (gs.id === 56) {
+      gs.title = "Midterm Grade";
+    }
+    res.send(gs);
+  } catch (err) {
+    res.status(500).send(err);
+    logger.error(err);
+  }
 });
 
-router.get("/gradeScheme", (req, res, next) => {
+router.get("/sectionTitles", async (req, res) => {
   const canvas = canvasAPI.getCanvasContext(req);
 
-  canvas.api
-    .get(`courses/${canvas.courseID}`)
-    .then(course =>
-      canvas.api
-        .get(`accounts/self/grading_standards/${course.grading_standard_id}`)
-        .catch(err =>
-          canvas.api.get(
-            `courses/${canvas.courseID}/grading_standards/${
-              course.grading_standard_id
-            }`
-          )
-        )
-    )
-    .then(gs => {
-      // Override titles for historic grade modes
-      if (gs.id === 40) {
-        gs.title = "Final Grade";
-      }
-      if (gs.id === 56) {
-        gs.title = "Midterm Grade";
-      }
-      return gs;
-    })
-    .then(gs => res.send(gs))
-    .catch(err => {
-      res.status(500).send(err);
-    });
-});
-
-router.get("/sectionTitles", (req, res, next) => {
-  const canvas = canvasAPI.getCanvasContext(req);
-
-  canvas.api
-    .get(`courses/${canvas.courseID}/sections`)
-    .then(sections => {
-      sections = sections.reduce((result, section) => {
+  try {
+    const sections = await canvas.api.get(
+      `courses/${canvas.courseID}/sections`
+    );
+    return res.send(
+      sections.reduce((result, section) => {
         result[section.sis_section_id] = section.name;
         return result;
-      }, {});
-      return res.send(sections);
-    })
-    .catch(err => {
-      res.status(500).send(err);
-    });
+      }, {})
+    );
+  } catch (err) {
+    return res.status(500).send(err);
+  }
 });
 
 module.exports = router;
