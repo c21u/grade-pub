4358986c109443dc370ae1023fe46356b9012b7a
D. Stuart Freeman <stuart.freeman@c21u.gatech.edu>
2025-12-15 09:34:11 -0500

    fix null dereference on checking if grades are loading

--- client/src/components/GradePublisher.js
+++ client/src/components/GradePublisher.js
@@ -580,7 +580,7 @@ const GradePublisher = (props) => {
                   clickHandler={bannerHandler}
                   dataReady={
                     canvasGrades &&
-                    canvasGrades[0].currentGrade !== "loading" &&
+                    canvasGrades[0]?.currentGrade !== "loading" &&
                     !needsAttendanceDates(true) &&
                     !schemeUnset
                   }
